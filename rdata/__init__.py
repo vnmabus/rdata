99837639fa1548653938141689eaf6704913d9a4
VNMabus <vnmabus@gmail.com>
2023-10-16 11:25:45 +0200

    Update project configuration.

--- rdata/__init__.py
+++ rdata/__init__.py
@@ -1,5 +1,4 @@
 """rdata: Read R datasets from Python."""
-import errno as _errno
 import os as _os
 import pathlib as _pathlib
 
@@ -16,14 +15,4 @@ Path of the test data.
 
 """
 
-try:
-    with open(
-        _pathlib.Path(_os.path.dirname(__file__)) / 'VERSION',
-        'r',
-    ) as version_file:
-        __version__ = version_file.read().strip()
-except IOError as e:
-    if e.errno != _errno.ENOENT:
-        raise
-
-    __version__ = "0.0"
+__version__ = "0.9.1.dev1"
