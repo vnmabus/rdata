9a539f458841d4fdfd7f782ac659c9868cef253a
Tuomas Rossi <tuomas.rossi@csc.fi>
2023-11-27 18:47:58 +0200

    Add compression

--- rdata/io/__init__.py
+++ rdata/io/__init__.py
@@ -1,4 +1,4 @@
-def write(fpath, data, format):
+def write(fpath, data, *, format, compression='gzip'):
     if format == 'ascii':
         from .ascii import WriterASCII as Writer
         mode = 'w'
@@ -7,6 +7,17 @@ def write(fpath, data, format):
         mode = 'wb'
     else:
         raise ValueError(f'Unknown format: {format}')
+
+    if compression == 'gzip':
+        from gzip import open
+    elif compression == 'bzip2':
+        from bz2 import open
+    elif compression == 'xz':
+        from lzma import open
+    else:
+        assert compression is None
+        import builtins
+        open = builtins.open
 
     with open(fpath, mode) as f:
         w = Writer(f)
